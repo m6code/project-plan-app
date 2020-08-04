82f5d3451c1d48908136cfcc317de30be1e41511
M6code <benjamynfrank101@gmail.com>
2020-08-04 11:36:54 +0100

    DashBoard Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './layout/NavBar';
+import DashBoard from './dashboard/DashBoard';
 
 function App() {
   return (
     <Router>
       <NavBar />
+      <Switch>
+        <Route path="/" component={DashBoard} />
+      </Switch>
     </Router>
   );
 }
