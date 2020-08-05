329e68b8e816ed1b429383578efecbb831073eec
M6code <benjamynfrank101@gmail.com>
2020-08-05 09:06:44 +0100

    Login & SignUp components

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './layout/NavBar';
 import DashBoard from './dashboard/DashBoard';
 import ProjectDetails from './projects/ProjectDetails';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={DashBoard} />
         <Route path="/project/:id" component={ProjectDetails} />
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
   );
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to="/">SignUp</NavLink></li>
-            <li><NavLink to="/">Login</NavLink></li>
+            <li><NavLink to="/sign-up">SignUp</NavLink></li>
+            <li><NavLink to="/sign-in">Login</NavLink></li>
         </ul>
     )
 }
