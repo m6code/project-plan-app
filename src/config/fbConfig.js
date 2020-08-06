cfcb35dea8a716307d853a0d6b1f5ca8d0c81f5f
M6code <benjamynfrank101@gmail.com>
2020-08-06 06:48:47 +0100

    Create Firebase Project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBFdNnDPrb8GbpaxHt_0cW6dd7_ow0gkxo",
+    authDomain: "project-planner-reactjs.firebaseapp.com",
+    databaseURL: "https://project-planner-reactjs.firebaseio.com",
+    projectId: "project-planner-reactjs",
+    storageBucket: "project-planner-reactjs.appspot.com",
+    messagingSenderId: "485893737253",
+    appId: "1:485893737253:web:8692234defbcf78fd7a8cc",
+    measurementId: "G-54HFYSH7DJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
