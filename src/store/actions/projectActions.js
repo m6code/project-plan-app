f9c0e3db0087e1119118ce3ea1065eb283bf434d
M6code <benjamynfrank101@gmail.com>
2020-08-08 12:44:16 +0100

    Connecting Redux to Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // Make async calls to db here
         dispatch({
             type: "CREATE_PROJECT",
