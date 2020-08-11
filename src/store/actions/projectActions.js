1e4e9345859faf51ca890b92083750a9b24eda20
M6code <benjamynfrank101@gmail.com>
2020-08-11 16:42:14 +0100

    Add data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,10 +1,21 @@
-
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // Make async calls to db here
-        dispatch({
-            type: "CREATE_PROJECT",
-            project: project
-        });
+
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: "Benjamin",
+            authorLastName: "SAO",
+            authorId: 12345,
+            createAt: new Date()
+        }).then(() => {
+            dispatch({
+                type: "CREATE_PROJECT",
+                project: project
+            });
+        }).catch((err) => {
+            dispatch({ type: "CREATE_PROJECT_ERROR", err})
+        })
     }
 }
